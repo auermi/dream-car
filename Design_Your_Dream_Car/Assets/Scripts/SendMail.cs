5486624ad8915ab8836bb9890d5c243dca842e33
CodeWritingCat <mauermcx@gmail.com>
2014-11-23 19:07:37 -0500

    added text-fields, email functionality, cleaned up object arrangement

---
+++ Design_Your_Dream_Car/Assets/Scripts/SendMail.cs
@@ -0,0 +1,48 @@
+﻿
+
+using System.Net;
+using System.Net.Mail;
+using UnityEngine.UI;
+
+using UnityEngine;
+
+public class SendMail : MonoBehaviour {
+	public string sender = "mauermcx@gmail.com";
+	public string receiver = "michaelandrewauer@outlook.com";
+	public string smtpPassword = "futurejag2014";
+	public string smtpHost = "smtp.gmail.com";
+	public GameObject mailButton;
+	
+	// Use this for initialization
+	private void Start() {
+		mailButton.GetComponent<Button>().onClick.AddListener( () => { mailTo(); });
+
+
+	}
+	private void mailTo() 
+	{
+		Application.CaptureScreenshot("Dream-Car.png");
+
+		using (var mail = new MailMessage {
+			From = new MailAddress(sender),
+			Subject = "test subject",
+			Body = "Hello there!"
+		}) {
+			mail.To.Add(receiver);
+
+
+			System.Net.Mail.Attachment attachment;
+			attachment = new System.Net.Mail.Attachment("Dream-Car.png");
+			mail.Attachments.Add(attachment);
+
+			var smtpServer = new SmtpClient(smtpHost) {
+				Port = 25,
+				Credentials = (ICredentialsByHost)new NetworkCredential(sender, smtpPassword)
+			};
+			ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+			smtpServer.EnableSsl = true;
+			smtpServer.Send(mail);
+		}
+		
+	}
+}
