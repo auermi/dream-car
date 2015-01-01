09867d6a1f8a8a43321887cb68ea89e705b27627
Michael Auer <mauermcx@gmail.com>
2015-01-01 14:37:22 -0500

    reworked the way the interface is parented and fixed other issues relating to it's visibility

--- Design_Your_Dream_Car/Assets/Scripts/InterfaceParenter.cs
+++ Design_Your_Dream_Car/Assets/Scripts/InterfaceParenter.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 
 public class InterfaceParenter : MonoBehaviour {
 
-	//Referencing the app slider which contains the script we want to access
-	public GameObject appSlider_ref;
+	//Tracking the slide number so we can hide the interface as necessary
+	private int scene_Index;
 
 	//Parents for keeping the dashboard hidden on the right screens
 	public GameObject hidden_Parent;
@@ -15,19 +15,54 @@ public class InterfaceParenter : MonoBehaviour {
 	//Interface container
 	public GameObject interface_Container;
 
-	/*First we grab the updated transition script value
-	Then, we check each frame to see if we need to change the visibility of the dashboard*/
-	void Update()
-	{
-		Transition transition_script = appSlider_ref.GetComponent<Transition>();
+	//Tracking scene Index with these buttons
+	public GameObject next_Button;
+	public GameObject previous_Button;
+	public GameObject restart_Button;
+	public GameObject start_Button;
 
-		if (transition_script.scene_index == 0 || transition_script.scene_index == 1 || transition_script.scene_index == 5)
+	//Event Listeners to track scene index and update interface visibility
+	void Start ()
+	{
+		scene_Index = 0;
+		CheckInterfaceVisibility ();
+		next_Button.GetComponent<Button> ().onClick.AddListener (() => { scene_Index++; CheckInterfaceVisibility(); });
+		previous_Button.GetComponent<Button>().onClick.AddListener ( () => { scene_Index--; CheckInterfaceVisibility();  });
+		restart_Button.GetComponent<Button> ().onClick.AddListener (() => { scene_Index = 0; CheckInterfaceVisibility(); });
+		start_Button.GetComponent<Button> ().onClick.AddListener (() => { scene_Index++; CheckInterfaceVisibility(); });
+	}
+
+	//Checking to see if the interface needs to be hidden or unhidden
+	//We parent flanking the ones we want to hide on so as to unhide the interface as needed
+	void CheckInterfaceVisibility()
+	{
+		switch (scene_Index) 
 		{
+		case 0:
 			interface_Container.transform.parent = hidden_Parent.transform;
-		}
-		else
-		{
+			break;
+		case 1:
+			interface_Container.transform.parent = hidden_Parent.transform;
+			break;
+		case 2:
 			interface_Container.transform.parent = interface_Parent.transform;
+			break;
+		case 4:
+			interface_Container.transform.parent = interface_Parent.transform;
+			break;
+		case 5:
+			interface_Container.transform.parent = hidden_Parent.transform;
+			break;
+		case 6:
+			interface_Container.transform.parent = interface_Parent.transform;
+			break;
+		case 11:
+			interface_Container.transform.parent = hidden_Parent.transform;
+			break;
+		//We only track up until 11 because the interface should stay hidden throughout the rest of the app
+		default:
+			break;
 		}
 	}
+
 }
