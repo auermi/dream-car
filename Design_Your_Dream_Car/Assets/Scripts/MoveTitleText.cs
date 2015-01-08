f2a228186b65f838521065390a3a2b17e62e1d0a
CodeWritingCat <mauermcx@gmail.com>
2015-01-08 03:06:02 -0500

    Completed implementing the info screen

--- Design_Your_Dream_Car/Assets/Scripts/MoveTitleText.cs
+++ Design_Your_Dream_Car/Assets/Scripts/MoveTitleText.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MoveTitleText : MonoBehaviour {
 
+	public GameObject titleText_Container;
+	private Vector3 scene11_position = new Vector3 (0f, 0f);
+	private Vector3 scene12_position = new Vector3 (0f, -300f);
+
+	public GameObject screen12;
+	public GameObject screen11;
+
+	//Tracking scene index here
+	private int sceneIndex;
+	public GameObject next_Button;
+	public GameObject previous_Button;
+	public GameObject done_Button;
+	public GameObject restart_Button;
+	public GameObject start_Button;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		sceneIndex = 0;
+		start_Button.GetComponent<Button> ().onClick.AddListener (() => {sceneIndex++; CheckToMoveTitleText();});
+		restart_Button.GetComponent<Button> ().onClick.AddListener (() => {sceneIndex = 0;CheckToMoveTitleText();});
+		done_Button.GetComponent<Button> ().onClick.AddListener (() => {sceneIndex = 0; CheckToMoveTitleText();});
+		next_Button.GetComponent<Button> ().onClick.AddListener (() => {sceneIndex++;  CheckToMoveTitleText();});
+		previous_Button.GetComponent<Button>().onClick.AddListener(()=> {sceneIndex--;CheckToMoveTitleText(); });
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void CheckToMoveTitleText()
+	{
+		if (sceneIndex == 12) {
+			titleText_Container.transform.parent = screen12.transform;
+			titleText_Container.transform.localPosition = scene12_position;
+		}
+		else
+		{
+			titleText_Container.transform.parent = screen11.transform;
+			titleText_Container.transform.localPosition = scene11_position;
+		}
 	}
+
 }
