ce0adece1671a1036f7a68ea6d6486d2445b09a9
CodeWritingCat <mauermcx@gmail.com>
2015-01-16 23:57:58 -0500

    added google analytics functionality to track sessions

---
+++ Design_Your_Dream_Car/Assets/AnalyticsTracking.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AnalyticsTracking : MonoBehaviour {
+
+	public GoogleAnalyticsV3 googleAnalytics;
+
+
+	public GameObject start_Button;
+	public GameObject restart_Button;
+	public GameObject done_Button;
+
+
+
+	// Use this for initialization
+	void Start () {
+		start_Button.GetComponent<Button>().onClick.AddListener(() => { googleAnalytics.StartSession (); });
+		restart_Button.GetComponent<Button> ().onClick.AddListener (() => {googleAnalytics.StopSession(); googleAnalytics.DispatchHits();});
+		done_Button.GetComponent<Button> ().onClick.AddListener (() => {googleAnalytics.StopSession(); googleAnalytics.DispatchHits();});
+	}
+	
+
+}
