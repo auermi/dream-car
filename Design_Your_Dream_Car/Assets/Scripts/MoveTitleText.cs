065e5269748ddf56fdbaa5a4b4a618980e829600
CodeWritingCat <mauermcx@gmail.com>
2015-01-27 13:17:15 -0500

    Text Fields Now Reset

--- Design_Your_Dream_Car/Assets/Scripts/MoveTitleText.cs
+++ Design_Your_Dream_Car/Assets/Scripts/MoveTitleText.cs
@@ -7,12 +7,12 @@ public class MoveTitleText : MonoBehaviour {
 	public GameObject titleText_Container;
 	private Vector3 scene11_position = new Vector3 (0f, 0f);
 	private Vector3 scene12_position = new Vector3 (0f, -300f);
+	
+	private GameObject name_Text;
+	private GameObject car_Text;
 
-	public GameObject name_TextField;
-	public GameObject car_TextField;
-
-	public GameObject name_Text;
-	public GameObject car_Text;
+	public GameObject name_text_field_prefab;
+	public GameObject car_text_field_prefab;
 
 	public GameObject screen12;
 	public GameObject screen11;
@@ -28,35 +28,55 @@ public class MoveTitleText : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
+		name_Text = Instantiate (name_text_field_prefab) as GameObject;
+		car_Text = Instantiate (car_text_field_prefab) as GameObject;
+		name_Text.transform.SetParent (titleText_Container.transform, false);
+		car_Text.transform.SetParent (titleText_Container.transform, false);
+		name_Text.transform.localPosition = new Vector3 (135f, 95f);
+		car_Text.transform.localPosition = new Vector3 (-135f, 95f);
 		sceneIndex = 0;
-		start_Button.GetComponent<Button> ().onClick.AddListener (() => {sceneIndex++; CheckToMoveTitleText();});
-		restart_Button.GetComponent<Button> ().onClick.AddListener (() => {sceneIndex = 0;CheckToMoveTitleText();  ResetTextFields();});
+		start_Button.GetComponent<Button> ().onClick.AddListener (() => {sceneIndex++; CheckToMoveTitleText(); });
+		restart_Button.GetComponent<Button> ().onClick.AddListener (() => {sceneIndex = 0;CheckToMoveTitleText();  ResetTextFields(); });
 		done_Button.GetComponent<Button> ().onClick.AddListener (() => {sceneIndex = 0; CheckToMoveTitleText(); ResetTextFields(); });
-		next_Button.GetComponent<Button> ().onClick.AddListener (() => {sceneIndex++;  CheckToMoveTitleText();});
-		previous_Button.GetComponent<Button>().onClick.AddListener(()=> {sceneIndex--;CheckToMoveTitleText(); });
+		next_Button.GetComponent<Button> ().onClick.AddListener (() => {sceneIndex++;  CheckToMoveTitleText(); });
+		previous_Button.GetComponent<Button>().onClick.AddListener(()=> {
+			sceneIndex--;
+			CheckToMoveTitleText(); 
+			if (sceneIndex < 11) { 
+				ResetTextFields();
+			} 
+		});
 	}
 	//Move title text and disable interactivity on the fields
 	void CheckToMoveTitleText()
 	{
 		if (sceneIndex == 12) {
-			titleText_Container.transform.parent = screen12.transform;
+			titleText_Container.transform.SetParent(screen12.transform, false);
 			titleText_Container.transform.localPosition = scene12_position;
-			name_TextField.GetComponent<InputField>().interactable = false;
-			car_TextField.GetComponent<InputField>().interactable = false;
+			name_Text.GetComponent<InputField>().interactable = false;
+			car_Text.GetComponent<InputField>().interactable = false;
 		}
 		else
 		{
 			titleText_Container.transform.parent = screen11.transform;
 			titleText_Container.transform.localPosition = scene11_position;
-			name_TextField.GetComponent<InputField>().interactable = true;
-			car_TextField.GetComponent<InputField>().interactable = true;
+			name_Text.GetComponent<InputField>().interactable = true;
+			car_Text.GetComponent<InputField>().interactable = true;
 		}
 	}
 
 	void ResetTextFields()
 	{
-		name_Text.GetComponent<Text>().text = "";
-		car_Text.GetComponent<Text>().text = "";
+		Destroy (name_Text);
+		Destroy (car_Text);
+		name_Text = Instantiate (name_text_field_prefab) as GameObject;
+		car_Text = Instantiate (car_text_field_prefab) as GameObject;
+		name_Text.transform.SetParent (titleText_Container.transform, false);
+		car_Text.transform.SetParent (titleText_Container.transform, false);
+		name_Text.transform.localPosition = new Vector3 (135f, 95f);
+		car_Text.transform.localPosition = new Vector3 (-135f, 95f);
+		Debug.Log("Reset The Field!");
 	}
+
 
 }
