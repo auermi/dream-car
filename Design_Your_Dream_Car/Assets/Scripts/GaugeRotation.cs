e612a75d76f35c2c61137b8b855c09967ccc1132
CodeWritingCat <mauermcx@gmail.com>
2015-01-27 23:40:54 -0500

    added new gauge icons

--- Design_Your_Dream_Car/Assets/Scripts/GaugeRotation.cs
+++ Design_Your_Dream_Car/Assets/Scripts/GaugeRotation.cs
@@ -143,20 +143,20 @@ public class GaugeRotation : MonoBehaviour {
 	{
 		total_Cost = fuel_Cost + drivetrain_Cost + transmission_Cost + bodyStyle_Cost + spoiler_Cost + wheels_Cost + paintCoat_Cost + decal_Cost;
 		float convertedTotal_Cost = total_Cost * -12f;
-		cost_Gauge.transform.eulerAngles = new Vector3 (0f, 0f, convertedTotal_Cost);
+		cost_Gauge.transform.eulerAngles = new Vector3 (0f, 0f, convertedTotal_Cost + 50f);
 	}
 	void CalculateFuelEfficiencyGauge()
 	{
 		total_Efficiency = fuel_Efficiency + drivetrain_Efficiency + transmission_Efficiency + bodyStyle_Efficiency + spoiler_Efficiency;
 		float convertedTotal_Efficiency = total_Efficiency * -18f;
-		efficiency_Gauge.transform.eulerAngles = new Vector3 (0f, 0f, convertedTotal_Efficiency);
+		efficiency_Gauge.transform.eulerAngles = new Vector3 (0f, 0f, convertedTotal_Efficiency + 50f);
 
 	}
 	void CalculateSpeedGauge()
 	{
 		total_Speed = fuel_Speed + transmission_Speed + bodyStyle_Speed + spoiler_Speed;
 		float convertedTotal_Speed = total_Speed * -22.5f;
-		speed_Gauge.transform.eulerAngles = new Vector3 (0f, 0f, convertedTotal_Speed);
+		speed_Gauge.transform.eulerAngles = new Vector3 (0f, 0f, convertedTotal_Speed+ 50f);
 	}
 	void ZeroGaugueValues()
 	{
