ea0b6037fdb3932ffc0592d2865239197e006c54
CodeWritingCat <mauermcx@gmail.com>
2015-03-02 23:22:04 -0500

    Good news everyone emailing is completely working on the ipads (Farnsworth)

--- Design_Your_Dream_Car/Assets/Scripts/SendMail.cs
+++ Design_Your_Dream_Car/Assets/Scripts/SendMail.cs
@@ -18,7 +18,7 @@ using System.IO;
 
 public class SendMail : MonoBehaviour {
 
-	public GameObject email_field_prefab;
+	//public GameObject email_field_prefab;
 	public MailMessage mail = new MailMessage();
 
 	public GameObject yes_button;
@@ -32,7 +32,7 @@ public class SendMail : MonoBehaviour {
 	private int sceneIndex;
 
 	//Textbox where we get the user's email
-	private GameObject email_TextBox;
+	public GameObject email_TextBox;
 
 	//Swap the parent of email_TextBox
 	public GameObject hidden_Parent;
@@ -41,79 +41,80 @@ public class SendMail : MonoBehaviour {
 	public GameObject email_Text;
 	public GameObject q_text;
 
+
+	public GameObject mailButton;
+
+	private string user_EmailAddress;
+
+
 	void Start()
 	{
-		email_TextBox = Instantiate(email_field_prefab) as GameObject;
+
+		//email_TextBox = Instantiate(email_field_prefab) as GameObject;
 		email_TextBox.transform.SetParent(hidden_Parent.transform); 
 		email_TextBox.transform.localPosition = new Vector3(550f, -510f);
-
 
 		yes_button.GetComponent<Button> ().onClick.AddListener (() => { yes_button.transform.SetParent(hidden_Parent.transform); email_TextBox.transform.SetParent(scene_13_Parent.transform); no_button.transform.SetParent(hidden_Parent.transform); q_text.transform.SetParent(hidden_Parent.transform);});
 		start_Button.GetComponent<Button> ().onClick.AddListener (() => {sceneIndex = 1; ;});
 
 		restart_Button.GetComponent<Button> ().onClick.AddListener (() => { 
-			Destroy(email_TextBox);
-			email_TextBox = Instantiate(email_field_prefab) as GameObject;
-			email_TextBox.transform.SetParent(hidden_Parent.transform); 
-			email_TextBox.transform.localPosition = new Vector3(550f, -510f);
-			yes_button.transform.SetParent(scene_13_Parent.transform); 
-			yes_button.transform.localPosition = new Vector3(416.32f, -150f);
-			no_button.transform.SetParent(scene_13_Parent.transform); 
-			no_button.transform.localPosition = new Vector3(609.4f, -150f);
-			q_text.transform.SetParent(scene_13_Parent.transform); 
-			q_text.transform.localPosition = new Vector3(513f, -22f);
-			CheckToMail(); 
-			sceneIndex = 0;
+			StartCoroutine(AttachAndMail());
 		});
 
 		next_Button.GetComponent<Button> ().onClick.AddListener (() => {sceneIndex++; CheckToScreenshot(); Debug.Log (sceneIndex);});
 		previous_Button.GetComponent<Button>().onClick.AddListener(()=> {yes_button.transform.SetParent(scene_13_Parent.transform); email_TextBox.transform.SetParent(hidden_Parent.transform); no_button.transform.SetParent(scene_13_Parent.transform); q_text.transform.SetParent(scene_13_Parent.transform); sceneIndex--;});
-		no_button.GetComponent<Button> ().onClick.AddListener (() => { yes_button.transform.SetParent(scene_13_Parent.transform); email_TextBox.transform.SetParent(hidden_Parent.transform); no_button.transform.SetParent(scene_13_Parent.transform); q_text.transform.SetParent(scene_13_Parent.transform); CheckToMail(); sceneIndex = 0; });
+		no_button.GetComponent<Button> ().onClick.AddListener (() => { yes_button.transform.SetParent(scene_13_Parent.transform); email_TextBox.transform.SetParent(hidden_Parent.transform); no_button.transform.SetParent(scene_13_Parent.transform); q_text.transform.SetParent(scene_13_Parent.transform); sceneIndex = 0; });
 	}
 
 	static void RemoveTakeScreenshot () {
-		//removes the file
+
 		Application.CaptureScreenshot("Dream-Car.png");
 		Debug.Log ("Screenshot!");
 	}
-	
-	void AttachAndMail(string emailAddress)
+	void EnterEmail() {}
+	IEnumerator AttachAndMail()
 	{
-		MailMessage mail = new MailMessage();
+		if (sceneIndex == 13) {
+						user_EmailAddress = email_Text.GetComponent<Text>().text;
+						MailMessage mail = new MailMessage ();
 		
-		mail.From = new MailAddress("imalabadmin@imamuseum.org");
-		mail.To.Add(emailAddress);
-		mail.Subject = "IMA Test Mail";
-		mail.Body = "Hello, attached is your Dream Car from the IMA Dream Car iOS App";
+						mail.From = new MailAddress ("imalabadmin@imamuseum.org");
+						mail.To.Add (user_EmailAddress);
+						mail.Subject = "IMA Test Mail";
+						mail.Body = "Hello, attached is your Dream Car from the IMA Dream Car iOS App";
 
-		System.Net.Mail.Attachment attachment;
-		attachment = new System.Net.Mail.Attachment(Application.persistentDataPath + "/Dream-Car.png");
-		mail.Attachments.Add(attachment);
+						System.Net.Mail.Attachment attachment;
+						attachment = new System.Net.Mail.Attachment (Application.persistentDataPath + "/Dream-Car.png");
+						mail.Attachments.Add (attachment);
 
-		SmtpClient smtpServer = new SmtpClient("smtp.mandrillapp.com");
-		smtpServer.Port = 587;
-		smtpServer.Credentials = new System.Net.NetworkCredential("imalabadmin@imamuseum.org", "") as ICredentialsByHost;
-		smtpServer.EnableSsl = true;
-		ServicePointManager.ServerCertificateValidationCallback = 
-			delegate(object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) 
-		{ return true; };
-		smtpServer.Send(mail);
-		Debug.Log("success");
-		
+						SmtpClient smtpServer = new SmtpClient ("smtp.mandrillapp.com");
+						smtpServer.Port = 587;
+						smtpServer.Credentials = new System.Net.NetworkCredential ("imalabadmin@imamuseum.org", "D32RGV9XlKVZlanXsh0pIg") as ICredentialsByHost;
+						smtpServer.EnableSsl = true;
+						ServicePointManager.ServerCertificateValidationCallback = 
+			delegate(object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) {
+								return true;
+						};
+						smtpServer.Send (mail);
+						yield return null;
+						Debug.Log ("success");
+						//Reset Email Screen
+						email_TextBox.transform.SetParent(hidden_Parent.transform); 
+						email_TextBox.transform.localPosition = new Vector3(550f, -510f);
+						yes_button.transform.SetParent(scene_13_Parent.transform); 
+						yes_button.transform.localPosition = new Vector3(416.32f, -150f);
+						no_button.transform.SetParent(scene_13_Parent.transform); 
+						no_button.transform.localPosition = new Vector3(609.4f, -150f);
+						q_text.transform.SetParent(scene_13_Parent.transform); 
+						q_text.transform.localPosition = new Vector3(513f, -22f);
+						sceneIndex = 0;
+						email_Text.GetComponent<Text>().text = "  ";
+						Application.LoadLevel(0);
+				}
 	}
-	
 
-	void CheckToMail()
-	{
-		if (sceneIndex == 13)
-		{
-			//string user_EmailAddress = email_Text.GetComponent<Text>().text;
-			//Debug.Log(user_EmailAddress);
-			//AttachAndMail(user_EmailAddress);
-			AttachAndMail("mauermcx@gmail.com");
-			//MailIt2();
-		}
-	}
+
+
 
 	void CheckToScreenshot()
 	{
